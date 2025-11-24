65f69dc83e7e2cc4c8d409a03c83749bab46a89c
oways-work <owayswork@gmail.com>
2025-11-24 19:52:05 +0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,8 +2,9 @@
 import sys
 import math
 
-# Check for argument to avoid crash
+# Check if the list has at least 2 items (script name + argument)
 if len(sys.argv) > 1:
     print(math.factorial(int(sys.argv[1])))
 else:
+    print("Error: You must provide a number.")
     print("Usage: ./factorial.py <number>")
