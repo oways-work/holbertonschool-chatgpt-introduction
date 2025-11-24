5cb5c9ba0a4ec184e8e8303822bf0c2aabda839c
oways-work <owayswork@gmail.com>
2025-11-24 19:48:52 +0300

    Use math.factorial instead of custom implementation
    Replaced custom factorial function with math.factorial for efficiency.

--- factorial.py
+++ factorial.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 import sys
+import math
 
-def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1  # <--- THIS WAS MISSING
-    return result
-
-# Check if an argument was provided to avoid an IndexError
+# Check for argument to avoid crash
 if len(sys.argv) > 1:
-    f = factorial(int(sys.argv[1]))
-    print(f)
+    print(math.factorial(int(sys.argv[1])))
 else:
     print("Usage: ./factorial.py <number>")
